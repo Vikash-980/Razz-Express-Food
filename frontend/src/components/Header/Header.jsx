b2213d4cbbd5c686735ae529140068e9439a3f8a
Vikash-980 <vikashkumar843399@gmail.com>
2026-01-30 23:06:52 +0530

    Second Commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import './Header.css'
 
-const Header = () => {
+// Yahan props mein setMenu ko receive karein
+const Header = ({ setMenu }) => {
   return (
-    <div>
-      <div className='header'>
-        <div className="header-contents">
-            <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featurin a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your dining experience, one delicious meal at a time.</p>
-            <button>View Menu</button>
-        </div>
+    <div className='header'>
+      <div className="header-contents">
+          <h2>Order your favourite food here</h2>
+          <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.</p>
+          
+          {/* href se scroll hoga aur onClick se navbar state update hogi */}
+          <a href='#explore-menu'>
+            <button onClick={() => setMenu("menu")}>View Menu</button>
+          </a>
       </div>
     </div>
   )
 }
 
 export default Header
+
+// import React from 'react'
+// import './Header.css'
+
+// const Header = () => {
+//   return (
+//     <div>
+//       <div className='header'>
+//         <div className="header-contents">
+//             <h2>Order your favourite food here</h2>
+//             <p>Choose from a diverse menu featurin a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your dining experience, one delicious meal at a time.</p>
+//             <button onClick={()=>setMenu("menu")} >View Menu</button>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Header
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,11 +3,25 @@ import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
 import { useNavigate } from 'react-router-dom';
 
-const Cart = () => {
+// setShowLogin ko props se liya
+const Cart = ({setShowLogin}) => {
 
-    const {cartItems, food_list, removeFromCart, getTotalCartAmount,url} = useContext(StoreContext);
+    // 1. Context se 'token' nikaala
+    const {cartItems, food_list, removeFromCart, getTotalCartAmount, url, token} = useContext(StoreContext);
 
     const navigate = useNavigate();
+
+    // 2. Checkout handle karne ke liye function
+    const handleCheckout = () => {
+        if (!token) {
+            // Agar user login nahi hai
+            setShowLogin(true);
+            window.scrollTo(0,0); // Page ke top par le jayega taaki popup dikhe
+        } else {
+            // Agar login hai toh seedha order page par
+            navigate('/order');
+        }
+    }
 
   return (
     <div className='cart'>
@@ -26,7 +40,7 @@ const Cart = () => {
             if(cartItems[item._id]>0)
             {
                 return (
-                    <div>
+                    <div key={index}>
                         <div className='cart-items-title cart-items-item'>
                             <img src={url+"/images/"+item.image} alt="" />
                             <p>{item.name}</p>
@@ -60,7 +74,8 @@ const Cart = () => {
                     <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
                 </div>
             </div>
-            <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+            {/* 3. onClick par naya function call kiya */}
+            <button onClick={handleCheckout}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
             <div>
@@ -77,3 +92,83 @@ const Cart = () => {
 }
 
 export default Cart
+
+// import React, { useContext } from 'react'
+// import './Cart.css'
+// import { StoreContext } from '../../context/StoreContext'
+// import { useNavigate } from 'react-router-dom';
+
+// const Cart = () => {
+
+//     const {cartItems, food_list, removeFromCart, getTotalCartAmount,url} = useContext(StoreContext);
+
+//     const navigate = useNavigate();
+
+//   return (
+//     <div className='cart'>
+//       <div className="cart-items">
+//         <div className="cart-items-title">
+//             <p>Item</p>
+//             <p>Title</p>
+//             <p>Price</p>
+//             <p>Quantity</p>
+//             <p>Total</p>
+//             <p>Remove</p>
+//         </div>
+//         <br />
+//         <hr />
+//         {food_list.map((item,index)=>{
+//             if(cartItems[item._id]>0)
+//             {
+//                 return (
+//                     <div>
+//                         <div className='cart-items-title cart-items-item'>
+//                             <img src={url+"/images/"+item.image} alt="" />
+//                             <p>{item.name}</p>
+//                             <p>${item.price}</p>
+//                             <p>{cartItems[item._id]}</p>
+//                             <p>${item.price*cartItems[item._id]}</p>
+//                             <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
+//                         </div>
+//                        <hr  />
+//                     </div>
+//                 )
+//             }
+//         })}
+//       </div>
+//       <div className="cart-bottom">
+//         <div className="cart-total">
+//             <h2>Cart Totals</h2>
+//             <div>
+//                 <div className="cart-total-details">
+//                     <p>Subtotal</p>
+//                     <p>${getTotalCartAmount()}</p>
+//                 </div>
+//                 <hr />
+//                 <div className="cart-total-details">
+//                     <p>Delivery Fee</p>
+//                     <p>${getTotalCartAmount()===0?0:2}</p>
+//                 </div>
+//                 <hr />
+//                 <div className="cart-total-details">
+//                     <b>Total</b>
+//                     <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+//                 </div>
+//             </div>
+//             <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+//         </div>
+//         <div className="cart-promocode">
+//             <div>
+//                 <p>If you have a promo code, Enter it here</p>
+//                 <div className='cart-promocode-input'>
+//                     <input type="text" placeholder='promo code'  />
+//                     <button>Submit</button>
+//                 </div>
+//             </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Cart
